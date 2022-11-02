c1da83ce3498dc7a622f494275fbb681781709e5
Aditya Prayaga <adityaprayaga@gmail.com>
2022-11-02 17:53:37 -0400

    Done Docs.

--- src/io/IReader.java
+++ src/io/IReader.java
@@ -5,5 +5,12 @@ import java.io.InputStream;
 
 public interface IReader<T> {
 
+  /**
+   * A read method to read from the given input stream and returns the read T object.
+   *
+   * @param inputStream contains the inputStream that needs to be read.
+   * @return Returns the read t object of type <T>.
+   * @throws IOException When unable to read from inputStream.
+   */
   T read(InputStream inputStream) throws IOException;
 }
--- src/io/IWriter.java
+++ src/io/IWriter.java
@@ -6,11 +6,11 @@ import java.io.OutputStream;
 public interface IWriter<T> {
 
   /**
-   * A write method
-   * 
-   * @param t
-   * @param outputStream
-   * @throws IOException
+   * A write method to write the given T object to the Provided OutputStream.
+   *
+   * @param t object t of DataType <T>.
+   * @param outputStream stream where the output of the t object is to be written.
+   * @throws IOException throws Exception when unable to write on the OutputStream is un accessible.
    */
   void write(T t, OutputStream outputStream) throws IOException;
 }
