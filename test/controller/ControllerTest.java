027e6c7896b2e22cc870849669882d0eb6f84918
Aditya Prayaga <adityaprayaga@gmail.com>
2022-11-02 16:29:38 -0400

    Tests fix.

--- test/controller/ControllerTest.java
+++ test/controller/ControllerTest.java
@@ -24,7 +24,7 @@ public class ControllerTest {
   private static final String MOCK_VIEW_SHOW_STRING = "MockView showString() called with String %s";
   private static final String MOCK_VIEW_SHOW_PROMPT = "MockView showPrompt() called with Key %s";
   private static final String MOCK_VIEW_SHOW_PORTFOLIO = "MockView showPortfolio() called with portfolio Name: %s";
-  private static final String MOCK_VIEW_SHOW_PORTFOLIO_VALUE = "MockView showPortfolioValue() called with portfolio Name: %s, Value: %.2d";
+  private static final String MOCK_VIEW_SHOW_PORTFOLIO_VALUE = "MockView showPortfolioValue() called with portfolio Name: %s, Value: %.2f";
 
 
   private static final String MOCK_MODEL_CREATE_PORTFOLIO =
@@ -161,6 +161,7 @@ public class ControllerTest {
               String.format(MOCK_VIEW_SHOW_PROMPT, "PROMPT_CHOICE"),
               String.format(MOCK_VIEW_SHOW_PROMPT, "PROMPT_PORTFOLIO_NAME"),
               String.format(MOCK_MODEL_READ_PORTFOLIO, FOUND_A_MATCH),
+              String.format(MOCK_VIEW_SHOW_PORTFOLIO,FOUND_A_MATCH),
               String.format(MOCK_VIEW_SHOW_OPTIONS, 0),
               String.format(MOCK_VIEW_SHOW_PROMPT, "PROMPT_CHOICE"),
               String.format(MOCK_VIEW_SHOW_STRING, Constants.EXITING_STATUS)
@@ -267,7 +268,7 @@ public class ControllerTest {
               String.format(MOCK_VIEW_SHOW_PROMPT, "PROMPT_PORTFOLIO_NAME"),
               String.format(MOCK_VIEW_SHOW_PROMPT, "PROMPT_DATE"),
               String.format(MOCK_MODEL_GET_PORTFOLIO_ON_DATE, FOUND_A_MATCH, ""),
-              String.format(MOCK_VIEW_SHOW_STRING, PORTFOLIO_VALUE),
+              String.format(MOCK_VIEW_SHOW_PORTFOLIO_VALUE, null,  PORTFOLIO_VALUE),
               String.format(MOCK_VIEW_SHOW_OPTIONS, 0),
               String.format(MOCK_VIEW_SHOW_PROMPT, "PROMPT_CHOICE"),
               String.format(MOCK_VIEW_SHOW_STRING, Constants.EXITING_STATUS)
@@ -342,7 +343,7 @@ public class ControllerTest {
               String.format(MOCK_VIEW_SHOW_PROMPT, "PROMPT_PORTFOLIO_NAME"),
               String.format(MOCK_VIEW_SHOW_PROMPT, "PROMPT_DATE"),
               String.format(MOCK_MODEL_GET_PORTFOLIO_ON_DATE, FOUND_A_MATCH, "2022-02-14"),
-              String.format(MOCK_VIEW_SHOW_STRING, 2 * PORTFOLIO_VALUE),
+              String.format(MOCK_VIEW_SHOW_PORTFOLIO_VALUE,null,2 * PORTFOLIO_VALUE),
               String.format(MOCK_VIEW_SHOW_OPTIONS, 0),
               String.format(MOCK_VIEW_SHOW_PROMPT, "PROMPT_CHOICE"),
               String.format(MOCK_VIEW_SHOW_STRING, Constants.EXITING_STATUS)
@@ -799,7 +800,7 @@ public class ControllerTest {
 
     @Override
     public void showPortfolioValue(Pair<Portfolio, Double> portfolioValue) {
-      this.log.add(MOCK_VIEW_SHOW_PORTFOLIO_VALUE,portfolioValue.getKey().getName(),portfolioValue.getValue());
+      this.log.add(String.format(MOCK_VIEW_SHOW_PORTFOLIO_VALUE,portfolioValue.getKey().getName(),portfolioValue.getValue()));
     }
   }
 }
